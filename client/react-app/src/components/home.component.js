c9983ed0444161aa1407f4a95805c4537f170bdf
Kiran A <kiranvegeta.1@gmail.com>
2020-07-19 03:30:10 +0530

    Home Page UI

--- client/react-app/src/components/home.component.js
+++ client/react-app/src/components/home.component.js
@@ -5,6 +5,9 @@ import {Link } from 'react-router-dom';
 import logo from '../reminder_logo.svg';
 
 function Home () {
+    var cardStyle = {
+        marginTop: "200px"
+    }
     const user = useSelector((state) => state.loginReducer)
     return <div>
             <nav class="navbar navbar-expand-sm bg-secondary fixed-top">
@@ -24,6 +27,53 @@ function Home () {
                     </li>
                 </ul>
             </nav>
+            
+  <div class="jumbotron d-flex align-items-center">
+  <div class="container">
+      <div>
+      <h1 class="text-center" style={{paddingTop: "100px"}}>Welcome Kiran</h1>
+  <div class="card-deck" style={cardStyle}>
+    <div class="card border-light mb-3 w-50">
+    <div class="card-header">
+    <h1>
+    <span class="badge badge-pill badge-info xl-2 col-sm-3 offset-md-4 ">3</span>
+        </h1>
+  </div>
+    <div class="card-body">
+      <h5 class="card-title">Important Tasks</h5>
+      <p class="card-text">Lists all the important tasks to be reminded.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+    <div class="card border-light mb-3 w-50">
+    <div class="card-header">
+    <h1>
+    <span class="badge badge-pill badge-info xl-2 col-sm-3 offset-md-4 ">3</span>
+        </h1>
+  </div>
+    <div class="card-body">
+      <h5 class="card-title">Important Tasks</h5>
+      <p class="card-text">Lists all the important tasks to be reminded.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+
+    <div class="card border-light mb-3 w-50">
+    <div class="card-header">
+    <h1>
+    <span class="badge badge-pill badge-info xl-2 col-sm-3 offset-md-4 ">3</span>
+        </h1>
+  </div>
+    <div class="card-body">
+      <h5 class="card-title">Important Tasks</h5>
+      <p class="card-text">Lists all the important tasks to be reminded.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+  </div>
+  </div>
+</div>
         </div>
         
 }
